d5730de76502d56b0bb0213b9a3090e9a2a2f466
seishonagon <paulatlan@fastmail.fm>
2015-05-11 23:40:04 +0200

    Commented the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is in response to the PA2 Assignment of the R Programming class
+## The file uses caching techniques to limit computation time for certain heavy operations. 
+## Objects (here a matrix) are replaced by lists containing the object and certain "methods" allowing
+## programmers to set and get it's value, and to set and get certain useful transformations.
+## In this instance, we cache the inverse of a matrix (found through the solve function)
 
-## Write a short comment describing this function
+## The first function creates the object (a CacheMatrix). As far as I can tell, the <<- assignment
+## allows the "subfunction" to "pull in" the variables from the parent 
+## environment. That's probably a complete misunderstanding of the way it 
+## actually works
+## The function is called with the actual matrix and returns a list object that can be
+## manipulated further.
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -18,7 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This functions accepts a CacheMatrix as an argument. If the inverse has 
+## already been solved, then we know it and it's stored, so we don't need to 
+## compute it. 
+## otherwise, just fire up the engine and solve away. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
