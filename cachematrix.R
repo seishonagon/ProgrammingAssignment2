27ac9c3c0a8806c7df3fcaa6473f7c906fb6ff8e
seishonagon <paulatlan@fastmail.fm>
2015-05-11 23:48:55 +0200

    second pass at commenting to clarify some points and conform to grading assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,19 @@
-## This function is in response to the PA2 Assignment of the R Programming class
+## This file is in response to the PA2 Assignment of the R Programming class
 ## The file uses caching techniques to limit computation time for certain heavy operations. 
-## Objects (here a matrix) are replaced by lists containing the object and certain "methods" allowing
-## programmers to set and get it's value, and to set and get certain useful transformations.
-## In this instance, we cache the inverse of a matrix (found through the solve function)
+## Objects (here, a matrix) are replaced by lists containing the object and certain "methods" allowing
+## programmers to set and get its value, and to set and get certain useful transformations.
+## In this instance, we cache the inverse of a matrix (found through the solve() function)
 
-## The first function creates the object (a CacheMatrix). As far as I can tell, the <<- assignment
+## The makeCacheMatrix() funciton creates the object (a CacheMatrix). 
+## As far as I can tell, the <<- assignment
 ## allows the "subfunction" to "pull in" the variables from the parent 
 ## environment. That's probably a complete misunderstanding of the way it 
-## actually works
+## actually works :-)
 ## The function is called with the actual matrix and returns a list object that can be
-## manipulated further.
+## manipulated further (and that contains NULL as the cached value of the inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
+      ## set a CacheMatrix object with a matrix, and methods to manipulate it programmatically
   inv <- NULL
   set <- function(y){
     x <<- y
@@ -26,10 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This functions accepts a CacheMatrix as an argument. If the inverse has 
-## already been solved, then we know it and it's stored, so we don't need to 
-## compute it. 
-## otherwise, just fire up the engine and solve away. 
+## The cacheSolve() function accepts a CacheMatrix as an argument. If the inverse has 
+## already been solved (i.e. the cached value of the inverse is non-NULL),  
+## then we just retrieve that, saving the computation time. 
+## Otherwise, just fire up the engine and solve away. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
